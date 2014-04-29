9d755718f02cdf185af58ab551dc2909effc6e95
Tim Phang <tphang@gmail.com>
2014-04-29 17:29:33 -0400

    formatter gives summary

--- plugin/rspec/vim_quickfix_formatter.rb
+++ plugin/rspec/vim_quickfix_formatter.rb
@@ -4,7 +4,7 @@ require 'rspec/core/formatters/base_text_formatter'
 class VimQuickfixFormatter < RSpec::Core::Formatters::BaseTextFormatter
   # This registers the notifications this formatter supports, and tells
   # us that this was written against the RSpec 3.x formatter API.
-  RSpec::Core::Formatters.register self, :dump_failures
+  RSpec::Core::Formatters.register self, :dump_failures, :dump_summary
 
   def initialize(output)
     super(output)
@@ -17,20 +17,22 @@ class VimQuickfixFormatter < RSpec::Core::Formatters::BaseTextFormatter
     end
   end
 
-  def dump_failure_info(example)
-    exception = example.exception
-    output.puts "#{relative_path(example.location)}: E: #{example.full_description}"
-    output.puts exception.message.gsub(/^\n/, '')
+  def dump_summary(summary)
+    output.puts "\n#{summary.summary_line}"
   end
 
   # NoOp
-  def dump_summary(*args); end
   def seed(*args); end
   def message(*args); end
   def dump_pending(*args); end
 
   private
 
+  def dump_failure_info(example)
+    exception = example.exception
+    output.puts "#{relative_path(example.location)}: E: #{example.full_description}"
+    output.puts exception.message.gsub(/^\n/, '')
+  end
 
   def relative_path(path)
     path.gsub(/^\.\//, '')
